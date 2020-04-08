d4eff083b837acef2aceb6cf3cbe1b3329b868dc
Michael Auderer <mike@silverstone.io>
2020-04-08 07:35:42 -0400

    Finish adding covid endpoints, add more unit testing

--- index.js
+++ index.js
@@ -7,8 +7,29 @@ function SymptomChecker(appId, apiToken) {
     diagnosis: async (body) => {
       const data = await authedApi('POST', 'diagnosis', body)
       return data
+    },
+    triage: async (body) => {
+      const data = await authedApi('POST', 'triage', body)
+      return data
+    },
+    search: async (query) => {
+      const data = await authedApi('GET', 'search', query)
+      return data
+    },
+    symptoms: async (query) => {
+      const data = await authedApi('GET', 'symptoms', query)
+      return data
+    },
+    riskFactors: async (query) => {
+      const data = await authedApi('GET', 'risk_factors', query)
+      return data
+    },
+    locations: async (query) => {
+      const data = await authedApi('GET', 'locations', query)
+      return data
     }
   }
 }
 
 module.exports = SymptomChecker
+
--- index.test.js
+++ index.test.js
@@ -15,6 +15,35 @@ describe('SymptomChecker', () => {
     })
     expect(res.question).toBeTruthy()
   })
+
+  test('triage', async () => {
+    const res = await checker.triage({
+      "sex": "male",
+      "age": 65,
+      "evidence": [
+         {"id": "p_16", "choice_id": "absent"},
+         {"id": "p_17", "choice_id": "present"},
+         {"id": "p_18", "choice_id": "absent"},
+         {"id": "p_19", "choice_id": "absent"},
+         {"id": "p_20", "choice_id": "absent"},
+         {"id": "p_21", "choice_id": "absent"},
+         {"id": "p_22", "choice_id": "absent"},
+         {"id": "s_0", "choice_id": "absent"},
+         {"id": "s_1", "choice_id": "absent"},
+         {"id": "s_2", "choice_id": "absent"},
+         {"id": "s_15", "choice_id": "present"},
+         {"id": "s_16", "choice_id": "present"},
+         {"id": "s_17", "choice_id": "absent"},
+         {"id": "s_18", "choice_id": "absent"},
+         {"id": "s_19", "choice_id": "absent"},
+         {"id": "s_20", "choice_id": "absent"},
+         {"id": "s_21", "choice_id": "absent"},
+         {"id": "p_12", "choice_id": "present"}
+        ]
+    })
+    console.log(res)
+    expect(res.triage_level).toBeTruthy()
+  })
 })
 
 
